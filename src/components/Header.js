6d0bdd047fd4868d4cd65a1960e60e2b630df791
Andre Fehrmann <andre.fehrmann.ext@syrocon.de>
2017-10-25 15:56:12 +0200

    implement routing. little restructuring, not finished. add favicon!

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => (
+    <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">Project name</a>
+            </div>
+            <div id="navbar" className="collapse navbar-collapse">
+                <ul className="nav navbar-nav">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/topics">Topics</Link></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './utilities/registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import Mainframe from './components/Router';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Mainframe />, document.getElementById('root'));
 registerServiceWorker();
