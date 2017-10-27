5e4fd01666c4aa0deac4a048639804fedefab0d1
Andre Fehrmann <andre.fehrmann.ext@syrocon.de>
2017-10-27 07:59:13 +0200

    add style to login modal.

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,6 +2,9 @@ import React from 'react'
 /*
 {window.btoa( "Baumhaus" )}
 */
+
+
+
 class Login extends React.Component {
 
     render() {
@@ -10,13 +13,17 @@ class Login extends React.Component {
 
                 <form className="form-signin">
                     <h2 className="form-signin-heading">Please sign in</h2>
-                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus="" />
-                    <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required="" />
+                    <div className="form-group">
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus="" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputPassword" className="sr-only">Password</label>
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required="" />
+                    </div>
                     <div className="checkbox">
-                    <label>
-                        <input type="checkbox" value="remember-me" /> Remember me
+                        <label>
+                            <input type="checkbox" value="remember-me" /> Remember me
                     </label>
                     </div>
                     <button className="btn btn-primary btn-block" type="submit">Sign in</button>
