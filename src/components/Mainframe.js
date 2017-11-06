d2af6a6e628043298547c0e78fbb71ee78958d47
Andre Fehrmann <andre.fehrmann.ext@syrocon.de>
2017-11-06 16:59:26 +0100

    first moment in redux setup without errors.

--- src/components/Mainframe.js
+++ src/components/Mainframe.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { loginUser, fetchQuote, fetchSecretQuote } from '../actions'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import Header from './Header'
 import Sitebar from './Sitebar'
 import Login from './../views/Login'
@@ -13,7 +17,7 @@ class Mainframe extends React.Component {
 
     this.state = {
       sitebar: getStorage("sitebar"),
-      loginSuccess: false
+      loginSuccess: true
     };
   }
 
@@ -24,15 +28,19 @@ class Mainframe extends React.Component {
     })
   }
 
+
   render() {
 
-    const application = <div className={(this.state.sitebar === "true") ? 'show container' : 'container'}>
-      <Route exact path="/" component={Home} />
-    </div>
-
+    const { dispatch, quote, isAuthenticated, errorMessage, isSecretQuote } = this.props
     const login = <Route path="/" component={Login} />
-
+    const application = ({ store }) => (
+      <div className={(this.state.sitebar === "true") ? 'show container' : 'container'}>
+        <Route exact path="/" component={Home} />
+      </div>
+    )
+    
     return (
+
       <Router>
         <div className="mainframe">
           <Header toggleMenu={() => this.toggleMenu()} renderOnLogin={this.state.loginSuccess} />
@@ -41,14 +49,34 @@ class Mainframe extends React.Component {
               <span className="sr-only">45% Complete</span>
             </div>
           </div>
-
           {this.state.loginSuccess && <Sitebar show={this.state.sitebar} />}
-
           {this.state.loginSuccess ? application : login}
         </div>
       </Router>
-    );
-  }
-};
+    )
+  };
+}
+Mainframe.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  quote: PropTypes.string,
+  isAuthenticated: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string,
+}
 
-export default Mainframe
+// These props come from the application's
+// state when it is started
+function mapStateToProps(state) {
+
+  const { quotes, auth } = state
+  const { quote, authenticated } = quotes
+  const { isAuthenticated, errorMessage } = auth
+
+  return {
+    quote,
+    isSecretQuote: authenticated,
+    isAuthenticated,
+    errorMessage
+  }
+}
+
+export default connect(mapStateToProps)(Mainframe)
