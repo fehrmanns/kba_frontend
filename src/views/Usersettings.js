6184c9f7fb6dace95003e17043f1b9e786d12d96
Andre Fehrmann <andre.fehrmann.ext@syrocon.de>
2017-11-08 09:42:47 +0100

    add all pages.

---
+++ src/views/Usersettings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Usersettings extends React.Component {
+
+    render() {
+        return (
+            <div className="starter-template">
+                <h1>Usersettings template</h1>
+                <p className="lead">Like you, I used to think the world was this great place where everybody lived by the same standards I did, then some kid with a nail showed me I was living in his world, a world where chaos rules not order, a world where righteousness is not rewarded. That's Cesar's world, and if you're not willing to play by his rules, then you're gonna have to pay the price.</p>
+            </div>
+        );
+    }
+}
