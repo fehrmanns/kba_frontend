10379c32b8504b83296d03342f7df9d07ee5b56a
Andre Fehrmann <andre.fehrmann.ext@syrocon.de>
2017-11-09 12:35:39 +0100

    tiny layout adjustments in the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FormattedMessage } from 'react-intl';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl'
+import { DropdownButton, MenuItem } from 'react-bootstrap'
+import './../css/header.css'
 //import FormattedDropDown from './FormattedDropDown'
 
 
@@ -25,9 +26,9 @@ class Header extends React.Component {
                         }
 
                         <Link className="navbar-brand" to="/"><FormattedMessage id="header.title" /></Link>
-
-                        <DropdownButton bsStyle="default" title={this.props.lang["header.dropdown.language."+this.props.language]} key={this.props.language} id={`dropdown-basic-${this.props.language}`} onSelect={(eventKey) => this.props.changeLanguage(eventKey)}>
-
+                    </div>
+                    <div className="button-container pull-right">
+                        <DropdownButton pullRight bsStyle="default" title={this.props.lang["header.dropdown.language." + this.props.language]} key={this.props.language} id={`dropdown-basic-${this.props.language}`} onSelect={(eventKey) => this.props.changeLanguage(eventKey)}>
                             <MenuItem eventKey="en"><FormattedMessage id="header.dropdown.language.en" /></MenuItem>
                             <MenuItem eventKey="de"><FormattedMessage id="header.dropdown.language.de" /></MenuItem>
                         </DropdownButton>
