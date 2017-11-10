0fa9d88ed5750c7b788b74782b0733faa4ede37f
Isidro Herreros-Jimenez <isidro.herreros-jimenez@syrocon.de>
2017-11-10 09:10:33 +0100

    introduced formatted input fields

---
+++ src/components/FormattedInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { injectIntl, intlShape } from 'react-intl';
+
+
+class FormattedInput extends React.Component {
+
+    render() {
+        //First get those props
+        const { intl, placeholder, ...rest } = this.props;
+        
+        //Pass placeholder and internationalize it
+        const fPlaceholder = intl.formatMessage({
+            id: placeholder,            
+            defaultMessage: ""
+        })
+        
+        return (
+            <input placeholder={fPlaceholder} {...rest} />            
+        );
+    }
+}
+
+FormattedInput.propTypes = {
+    intl: intlShape.isRequired
+};
+
+export default injectIntl(FormattedInput);
