0a086712f511bd25b2133c9db6a877402965445b
Hector <hmvalcarcel@gmail.com>
2018-01-23 10:49:14 +0100

    frontend: Update eslint configuration
    - Remove rules for events on html5 elements
    - Remove import plugins to allow ES6 import/export styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "extends": "airbnb",
 
-    "plugins":[
+    "plugins": [
         "react",
         "jsx-a11y",
     ],
@@ -15,28 +15,30 @@ module.exports = {
         }
     },
 
-    "rules":{
-    "object-curly-spacing":0,
-    "react/jsx-filename-extension":0,
-    "indent": [2,4, {"SwitchCase":1}],
-    "quotes": ["warn", "double"],
-    "react/jsx-indent":[2,4],
-    "react/jsx-indent-props": [2,4],
-    "max-len": [1, 200],
-    "react/prefer-stateless-function":0,
-    "jsx-a11y/anchor-is-valid":0,
-    "linebreak-style": ["error", "unix"],
-    "no-multiple-empty-lines":["error", {"max":2}],
-    "react/forbid-prop-types": [2, {"forbid":["any"]}],
-    "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }],
+    "rules": {
+        "object-curly-spacing": 0,
+        "react/jsx-filename-extension": 0,
+        "indent": [2, 4, {"SwitchCase": 1}],
+        "quotes": ["warn", "double"],
+        "react/jsx-indent": [2, 4],
+        "react/jsx-indent-props": [2, 4],
+        "max-len": [1, 200],
+        "react/prefer-stateless-function": 0,
+        "jsx-a11y/anchor-is-valid": 0,
+        "linebreak-style": ["error", "unix"],
+        "no-multiple-empty-lines": ["error", {"max": 2}],
+        "react/forbid-prop-types": [2, {"forbid": ["any"]}],
+        "no-unused-expressions": ["error", {"allowShortCircuit": true, "allowTernary": true}],
+        "jsx-a11y/click-events-have-key-events": 0,
+        "jsx-a11y/no-static-element-interactions": 0
     },
 
     "globals": {
-        "window":true,
+        "window": true,
     },
 
-    "env":{
-        "browser":true,
+    "env": {
+        "browser": true,
         "es6": true,
         "node": true,
         "jest": true,
